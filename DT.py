47024e30c3456f820498c4332c107d772fb48103
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-04 19:12:03 +0100

    Data file and reading the data into the data frame object

--- DT.py
+++ DT.py
@@ -3,3 +3,8 @@ import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
+#=========================Importing the dataframe and reading it into the project=========================
+my_data = pd.read_csv("./drug200.csv", delimiter=",")
+my_data[0:5]
+df = my_data.head()
+print(df)
