16c331bbd98be54107fad76f2b441cd260568be0
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-04 19:07:38 +0100

    Importing the needed packages

---
+++ DT.py
@@ -0,0 +1,5 @@
+#===========================IMPORTING LIBRARIES AND PACKAGES, SOURCE FILES============================
+import numpy as np 
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
