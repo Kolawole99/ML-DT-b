c7d68a5b5d83ec8f9dc316e055203d851cf1b0d5
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-04 19:17:49 +0100

    Getting the data size

--- DT.py
+++ DT.py
@@ -8,3 +8,9 @@ my_data = pd.read_csv("./drug200.csv", delimiter=",")
 my_data[0:5]
 df = my_data.head()
 print(df)
+
+#======================================Getting the data size===================================
+dsize = df.shape
+print(dsize)
+
+#==========================
