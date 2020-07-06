683ce6ed98667d199ba48ccc77a8d9d33645858c
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-06 10:58:46 +0100

    Converted all categoriacl values to numerical for use in decision trees

--- DT.py
+++ DT.py
@@ -1,6 +1,7 @@
 #===========================IMPORTING LIBRARIES AND PACKAGES, SOURCE FILES============================
 import numpy as np 
 import pandas as pd
+from sklearn import preprocessing
 from sklearn.tree import DecisionTreeClassifier
 
 #=========================Importing the dataframe and reading it into the project=========================
@@ -20,5 +21,23 @@ X = my_data[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']].values
 pre = X[0:5]
 print(pre)
 
-#=================================
+#============================Convert categorical variables to numerical values=============================
+le_sex = preprocessing.LabelEncoder()
+le_sex.fit(['F','M'])
+X[:,1] = le_sex.transform(X[:,1]) 
+
+
+le_BP = preprocessing.LabelEncoder()
+le_BP.fit([ 'LOW', 'NORMAL', 'HIGH'])
+X[:,2] = le_BP.transform(X[:,2])
+
+
+le_Chol = preprocessing.LabelEncoder()
+le_Chol.fit([ 'NORMAL', 'HIGH'])
+X[:,3] = le_Chol.transform(X[:,3]) 
+
+X[0:5]
+preprocessed = X[0:5]
+print(preprocessed)
+
 
